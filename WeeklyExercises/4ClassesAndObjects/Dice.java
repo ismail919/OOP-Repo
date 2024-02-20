049a8c7d83812228dc1c5a27620200d335aa8aad
ismail919 <I.khan8993@student.leedsbeckett.ac.uk>
2024-02-20 11:35:47 +0000

    Complete

--- WeeklyExercises/4ClassesAndObjects/Dice.java
+++ WeeklyExercises/4ClassesAndObjects/Dice.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Dice {
-    public static void main (String[] args)
-    {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Random Random = new Random();
+        Random random = new Random();
 
-        // Prompt the user for the number of sides for two dice
-         System.out.print("Enter the number of sides for each die: ");
-         int sides = scanner.nextInt();
+        // Prompt the user for the number of sides for each die
+        System.out.print("How many sides does die 1 have? ");
+        int sidesDie1 = scanner.nextInt();
+        System.out.print("How many sides does die 2 have? ");
+        int sidesDie2 = scanner.nextInt();
 
         // Variables to keep track of sum and count of rolls for each die
         int sumDie1 = 0, sumDie2 = 0;
@@ -18,21 +19,22 @@ public class Dice {
         // "Roll" the dice three times
         for (int i = 0; i < countRolls; i++) {
             // Generate random numbers for each die
-            int rollDie1 = Random.nextInt(sides) + 1;
-            int rollDie2 = Random.nextInt(sides) + 1;
+            int rollDie1 = random.nextInt(sidesDie1) + 1;
+            int rollDie2 = random.nextInt(sidesDie2) + 1;
 
             // Update the sum for each die
             sumDie1 += rollDie1;
             sumDie2 += rollDie2;
 
             // Output the result of each roll
-            System.out.println("Roll " + (i + 1) + ": Die 1 = " + rollDie1 + ", Die 2 = " + rollDie2);
+            System.out.println("Die 1 roll " + (i + 1) + " = " + rollDie1);
+            System.out.println("Die 2 roll " + (i + 1) + " = " + rollDie2);
         }
 
         // Calculate and output the average for each die
         double avgDie1 = (double) sumDie1 / countRolls;
         double avgDie2 = (double) sumDie2 / countRolls;
-        System.out.println("\nAverage for Die 1: " + avgDie1);
-        System.out.println("Average for Die 2: " + avgDie2);
+        System.out.println("Die 1 rolled a total of " + sumDie1 + " and rolled " + avgDie1 + " on average.");
+        System.out.println("Die 2 rolled a total of " + sumDie2 + " and rolled " + avgDie2 + " on average.");
     }
 }
